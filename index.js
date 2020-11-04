ed7893b5cec5bc043806720f990e539be7f47f34
Your Name <you@example.com>
2020-11-04 14:34:55 -0600

    8th - 13 passed

--- index.js
+++ index.js
@@ -215,11 +215,11 @@ Using the miles function below do the following:
   3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-    /*add your code here*/
+function miles(km){
+    km = km * 0.621371;
+    return km;
   }
-
-
+console.log(miles);
 
 //Task 5b - Feet to CM
 /*
