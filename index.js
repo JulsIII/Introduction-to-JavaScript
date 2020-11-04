20368bd7823df443ff50b962c70e99b21846015a
Your Name <you@example.com>
2020-11-04 14:44:24 -0600

    10th - 15 passed

--- index.js
+++ index.js
@@ -246,8 +246,9 @@ Using the annoyingSong function below do the following:
       "(number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/){
-        /*add your code here*/
+function annoyingSong(Number){
+        let startNumber = (Number + " bottles of soda on the wall, " + Number + " bottles of soda, take one down pass it around " + (Number - 1) +" bottles of soda on the wall");
+        return startNumber;
   }
 
 
